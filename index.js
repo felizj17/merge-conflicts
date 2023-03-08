2eb3f61364a288ac27ec264f90d649e9110836fe
Felizj17 <felizj17@gmail.com>
2023-03-07 21:37:28 -0500

    made more changes

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-function (...c) {
-  if (!c.every((a) => Number.isNumber(a)))
+function random(...nums) {
+  if (!nums.every((a) => Number.isNumber(a)))
     throw "All arguments must be numbers.";
-  return c.reduce((a, b) => a + b);
+  return nums.reduce((num1, num2) => num1 + num2);
 }
 
-fn(1, 2, 3); //> 6
+bumber(1, 2, 3); //> 6
 fn(10, "B", 20); //> error All arguments must be numbers.
